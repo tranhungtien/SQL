71a6edb1789ffc9d59f24d20bc3ea86a64db9375
Tran Hung Tien <hungtien2202@gmail.com>
2026-09-25 15:07:30 +0700

    feat: handle number or string input

--- elements.sh
+++ elements.sh
@@ -9,4 +9,11 @@ PSQL="psql -h localhost -p 5432 -U postgres -d bikes -X --tuples-only -c"
 if [[ ! $1 ]] 
 then
 	echo "Please provide an element as an argument."
+else 
+	if [[ $1 =~ ^[0-9]+$ ]]
+	then 
+		CONDITION="atomic_number = $1"
+	else 
+		CONDITION="symbol = '$1' OR name = '$1'"
+	fi
 fi
