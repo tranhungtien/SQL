d68df6859976b2416409908c345c4de0f375c60b
Tran Hung Tien <hungtien2202@gmail.com>
2026-09-27 00:42:06 +0700

    feat: handle user doesnt exist

--- number guessing game/number_guess.sh
+++ number guessing game/number_guess.sh
@@ -9,3 +9,10 @@ PSQL="psql -h localhost -p 5432 -U postgres -d number_guess -X --no-align --tupl
 
 echo "Enter your username:"
 read USER_NAME
+
+if [[ -z $GET_USER_ID ]]
+then
+  	echo "Welcome, $USER_NAME! It looks like this is your first time here."
+  	ADD_NEW_USER=$($PSQL "INSERT INTO users(user_name) VALUES('$USER_NAME');")
+  	GET_USER_ID=$($PSQL "SELECT user_id FROM users WHERE user_name = '$USER_NAME';")
+fi
