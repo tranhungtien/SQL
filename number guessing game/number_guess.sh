f37ab2acd683a26647c07a1fb8a5255f66ee7e2e
Tran Hung Tien <hungtien2202@gmail.com>
2026-09-27 00:44:58 +0700

    feat: handle user name exist and output game played information

--- number guessing game/number_guess.sh
+++ number guessing game/number_guess.sh
@@ -10,9 +10,16 @@ PSQL="psql -h localhost -p 5432 -U postgres -d number_guess -X --no-align --tupl
 echo "Enter your username:"
 read USER_NAME
 
+GET_USER_ID=$($PSQL "SELECT user_id FROM users WHERE user_name = '$USER_NAME';")
+
 if [[ -z $GET_USER_ID ]]
 then
   	echo "Welcome, $USER_NAME! It looks like this is your first time here."
   	ADD_NEW_USER=$($PSQL "INSERT INTO users(user_name) VALUES('$USER_NAME');")
   	GET_USER_ID=$($PSQL "SELECT user_id FROM users WHERE user_name = '$USER_NAME';")
+else
+	GET_INFO_GAME=$($PSQL "SELECT COUNT(game_id), MIN(moves) FROM games WHERE user_id = $GET_USER_ID;")
+  	IFS='|' read TOTAL_GAMES BEST_GAME <<< "$GET_INFO_GAME"
+  	#echo "$TOTAL_GAMES $BEST_GAME"
+  	echo "Welcome back, $USER_NAME! You have played $TOTAL_GAMES games, and your best game took $BEST_GAME guesses."
 fi
