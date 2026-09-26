081b8d881770b6ed043024f2d6e0875be23add66
Tran Hung Tien <hungtien2202@gmail.com>
2026-09-27 00:40:15 +0700

    feat: get user name

--- number guessing game/number_guess.sh
+++ number guessing game/number_guess.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+if [ -f .env ]; then
+	export $(grep -v '^#' .env | xargs)
+fi
+
+PSQL="psql -h localhost -p 5432 -U postgres -d number_guess -X --no-align --tuples-only -c"
+
+
+echo "Enter your username:"
+read USER_NAME
