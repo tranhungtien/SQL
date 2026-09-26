6d67f7858432e64fc54c6114413c18cb3f279c95
Tran Hung Tien <hungtien2202@gmail.com>
2026-09-27 00:46:20 +0700

    feat: generate secret key

--- number guessing game/number_guess.sh
+++ number guessing game/number_guess.sh
@@ -6,6 +6,7 @@ fi
 
 PSQL="psql -h localhost -p 5432 -U postgres -d number_guess -X --no-align --tuples-only -c"
 
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 
 echo "Enter your username:"
 read USER_NAME
